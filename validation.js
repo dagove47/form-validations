d5db69db15c30380a176e49983b812157b537793
KevinJPC <kepc08@gmail.com>
2021-01-27 21:40:24 +0000

    update

--- validation.js
+++ validation.js
@@ -1,4 +1,10 @@
 function formValidation(event) {
+    event.preventDefault();
+    let card = document.getElementById ("inputCard").value;
+    let myalert = document.getElementById("alert");
 
-
+    if(!(card.length == 16)){
+        myalert.style.display = "block"
+    }
 }
+
